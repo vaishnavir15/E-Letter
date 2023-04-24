d536f945d9f9dd535e8a0a036ca54046c466841a
Vaishnavi Ratnasabapathy <vratnasa@uwaterloo.ca>
2023-04-24 19:49:11 -0400

    made a connection for front and back

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 //import AboutPage from './components/AboutPage';
 import HomePage from './components/HomePage';
-//import CreatePage from './components/CreatePage';
-function App(){
-  return (<div><HomePage /></div>);
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      < HomePage/>
+    </div>
+  );
 }
 
 export default App;
