ccd6933459a1a7a6ecf322b414f4fb921132e7b7
Vaishnavi Ratnasabapathy <vratnasa@uwaterloo.ca>
2023-04-25 21:15:13 -0400

    can store images that were uploaded

--- server/server.js
+++ server/server.js
@@ -1,73 +1,30 @@
-// server.js
-
-const express = require('express');
-const multer = require('multer');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/ELetter', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error(error));
-
-// Define a schema for images
-const imageSchema = new mongoose.Schema({
-  filename: String,
-  url: String,
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-});
-
-// Create a model for images
-const Image = mongoose.model('Image', imageSchema);
-
-// Configure multer middleware for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
+
 const upload = multer({ storage: storage });
 
-// Middleware to ensure user is authenticated
-function authMiddleware(req, res, next) {
-  if (!req.user) {
-    res.status(401).send('Not authorized');
-  } else {
-    next();
-  }
-}
-
-// Upload endpoint
-app.post('/upload', authMiddleware, upload.single('image'), async (req, res) => {
-  const file = req.file;
-
-  // Save the uploaded image to MongoDB
-  const newImage = new Image({
-    filename: file.filename,
-    url: `/uploads/${file.filename}`,
-    owner: req.user._id,
-  });
-  await newImage.save();
-
-  res.send('Image uploaded successfully');
+app.post("/api/upload-image", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.send("File uploaded successfully");
 });
 
-// Image retrieval endpoint
-app.get('/uploads/:userId', async (req, res) => {
-  const images = await Image.find({ owner: req.params.userId });
-  res.json(images);
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
   
 
--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -1,66 +1,67 @@
-import React, {useState} from 'react'
-import Navbar from './Navbar';
-import UploadButton from './images/UploadButton.png'
-import EmailSun from './images/EmailSun.jpg'
-import '../styles.css'
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
+import UploadButton from "./images/UploadButton.png";
+import EmailSun from "./images/EmailSun.jpg";
+import "../styles.css";
 
-function UploadPage(){
-    const [fileInput, setFileInput] = useState(null);
+function UploadPage() {
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    function fileSelectedHandler(event)
-    {
-        console.log(event.target.files[0]);
-    }
-    function fileUploadHandler(){
-        fileInput.click();
-    }
-    return(
-        <div>
-            <header>
-                <Navbar></Navbar>
-                <div className="yellow-header">
-                    <h1 className="middle about-title">E-Letters</h1>
-                    <h2 className="middle about">UPLOAD</h2>
-                </div>
-                <div className="upside-down-triangle-upload">
-                </div>
-                
-            </header>
-            <section>
-            <div className="middle-over home-icon"> 
-                <img src= {EmailSun} width="250" alt=""></img>
-            </div>
-            <div>
-                <button className="pick-button" onClick={fileUploadHandler}>Pick </button>
-            </div>
-            <div>
-                <input 
-                style={{display:"none"}}
-                type="file" 
-                onChange={fileSelectedHandler}
-                ref={input => setFileInput(input)} >
-                
-                </input>
-                
-            </div>
-            <div className="middle sub-button-upload">
-                <button onClick={fileUploadHandler} className="button-upload">
-                    <img src= {UploadButton} width="250" alt=""></img>
-                </button>
-                
-            </div>
-                
-            </section>
-            <footer className="footer middle">
-                <hr></hr>
-                <p>
-                    Have cool photos you want others to be able to use? Upload them here for others to use!
-                </p>
-            </footer>
+  const fileSelectedHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const fileUploadHandler = () => {
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    axios.post("http://localhost:5000/api/upload-image", formData).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+
+  return (
+    <div>
+      <header>
+        <Navbar />
+        <div className="yellow-header">
+          <h1 className="middle about-title">E-Letters</h1>
+          <h2 className="middle about">UPLOAD</h2>
         </div>
-
-    ); 
+        <div className="upside-down-triangle-upload"></div>
+      </header>
+      <section>
+        <div className="middle-over home-icon">
+          <img src={EmailSun} width="250" alt="" />
+        </div>
+        <div>
+          <button className="pick-button" onClick={() => {document.getElementById("fileInput").click()}}>
+            Pick
+          </button>
+          <input
+            style={{ display: "none" }}
+            type="file"
+            id="fileInput"
+            onChange={fileSelectedHandler}
+          />
+        </div>
+        <div className="middle sub-button-upload">
+          {selectedFile && (
+            <button onClick={fileUploadHandler} className="button-upload">
+              <img src={UploadButton} width="250" alt="" />
+            </button>
+          )}
+        </div>
+      </section>
+      <footer className="footer middle">
+        <hr />
+        <p>Have cool photos you want others to be able to use? Upload them here for others to use!</p>
+      </footer>
+    </div>
+  );
 }
 
-export default UploadPage
+export default UploadPage;
